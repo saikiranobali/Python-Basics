95773faacee88bfbbc2b78e5c72a7861200b22a8
saikiranobali <164886969+saikiranobali@users.noreply.github.com>
2024-05-13 22:09:02 +0530

    Add files via upload

---
+++ Range().py
@@ -0,0 +1,11 @@
+#Range Function and For Loop in Python
+#Generally range() takes three parameters as start,end,step.It iterates from start point to end-1 with step size
+for i in range(2,11,2):
+    print(i)
+
+#Output
+2
+4
+6
+8
+10
---
+++ While_Loop.py
@@ -0,0 +1,17 @@
+#While Loop in Python
+count=0
+while count<10:
+    count+=1
+    print("Rohit Sharma")
+
+#Output
+# Rohit Sharma
+# Rohit Sharma
+# Rohit Sharma
+# Rohit Sharma
+# Rohit Sharma
+# Rohit Sharma
+# Rohit Sharma
+# Rohit Sharma
+# Rohit Sharma
+# Rohit Sharma
